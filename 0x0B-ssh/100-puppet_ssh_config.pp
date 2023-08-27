c5c9a31a53232367284b27f7eb8ebde3509bd3bc
rodgersxy <rodgersny99@gmail.com>
2023-08-27 02:30:36 -0700

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
 # Puppet to make changes to our configuration file.
-file { 'disable password for login':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-}
-file { 'Path to find keys':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+file { 'SSH client configuration':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  content => "
+    Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+    ",
 }
