d098ca2a81cdd62c80982875aa68c093fd2eb2ca
rodgersxy <rodgersny99@gmail.com>
2023-08-29 14:33:26 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,25 @@
-# Configuring your server with Puppet
-# Ensure nginx service is running and enabled
-# Configure nginx to listen on port 80
-
+# Ensure nginx package is installed
 package { 'nginx':
-  ensure   => 'installed',
+  ensure  => 'installed',
 }
+
+# Configure Nginx to listen on port 80
 file { 'first content':
   ensure  => present,
   path    => '/var/www/html/index.html',
   content => 'Hello World!',
+}
 
+# Configure redirection
 file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://github.com/rodgersxy permanent;',
-}
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://github.com/rodgersxy permanent;',
+  require => Package['nginx'],  # This line ensures that the package is installed before this resource is managed
 }
 
+# Ensure Nginx service is running and enabled
 service { 'nginx':
   ensure  => running,
   enable  => true,
