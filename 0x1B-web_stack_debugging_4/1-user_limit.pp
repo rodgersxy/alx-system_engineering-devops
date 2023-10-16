1782c367ef813e8e4737b0e8c9322a46afb26d39
rodgersxy <rodgersny99@gmail.com>
2023-10-16 17:55:38 +0000

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# To login with the holberton user and open a file without any error message.
+
+exec { 'hard-file-increase':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft-file-increase':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
